a17089cd0b046ad8d9df15a3720f8d81200daf4f
KK <kk.contact.mail@gmail.com>
2019-03-08 18:01:39 +0100

    Started with circle detection

--- DetectMarks/DetectMarks.cpp
+++ DetectMarks/DetectMarks.cpp
@@ -19,6 +19,7 @@
 using namespace cv;
 using namespace std;
 
+
 Mat src; Mat src_gray;
 int thresh = 100;
 int max_thresh = 255;
@@ -274,7 +275,7 @@ int main2()
 	return 0;
 }
 
-int main()
+int main3()
 {
 	cv::String algo = "bm"; // stereo matching method (bm or sgbm)
 	int max_disp = 160; //ui->horizontalSlider_num_of_disparity->value();
@@ -410,3 +411,142 @@ int main()
 // https://github.com/opencv/opencv_contrib/blob/master/modules/ximgproc/samples/disparity_filtering.cpp
 // Disparity
 // https://docs.opencv.org/3.1.0/d3/d14/tutorial_ximgproc_disparity_filtering.html  ***
+
+int main()
+{
+
+	Mat left_image = imread(".\\pics\\cir-ts-03.jpg");//(".\\TS01.jpg", 1);
+
+	//Mat left_image = imread(".\\pics\\stereo\\stereo07L.jpg");//(".\\TS01.jpg", 1);
+	Mat right_image = imread(".\\pics\\stereo\\stereo07R.jpg");//(".\\TS01.jpg", 1);
+
+
+	
+
+	if (left_image.empty()) { printf("Could not open left image file!!\n"); getchar(); exit(-1); }
+	if (right_image.empty()) { printf("Could not open right image file!!\n"); getchar(); exit(-1); }
+	
+	Mat src_gray; // 1st gray
+	Mat src_blur; // 2nd blur (see guassian?)
+	Mat src_edge; // canny
+	Mat src_Hough; // Hough
+	
+	// Edge detection
+	// Blur image to reduce noise
+	int iBlurKernelSize = 5;
+
+	// Canny
+	// 1. Fiter out any noise. Gaussian filter.
+	// 2. Find the intensity gradient of the image. Analogous to Sobel. 
+	//    Gradient strength G and direction Theta. 
+	//    The direction Theta is rounded to four possible angles, 0, 45, 90 or 135
+	int kernel_size = 3;   // the size of the Sobel kernel to be used internally
+	// 3. Non-maximum supression is applied to remove pixels that are not considered to be part of an edge.
+	//    Hence only thin lines (candidate edge) will remain.
+	// 4. Hysteresis: The final step. Canny does use two thresholds (upper and lower).
+	//    a. If a pixel gradient is higher than the upper threshold, the pixel is accepted as an edge
+	//    b. If a pixel gradient value is below the lower threshold, then it is rejected.
+	//    c. If the pixel gradient is between the two thresholds, then it will be accepted only if it is connected to a pixel that is above the upper threshold.
+	//    Canny recommended a upper : lower ratio between 2 : 1 and 3 : 1.
+	//    AA: Experment shows higher (3) is better in reducing noise and detecint traffic object.
+	int lowThreshold = 50; // if pixel gradient is below the lower threshold, then it is rejected.
+	int ratio = 3;         // if pixel gradient is above ratio x lowThreshold, then it is accepted.
+	
+
+	// For control window
+	int const max_lowThreshold = 100; // Control window. Max lowThreshold 
+
+    /// Create Window
+	char source_window[] = "Source";
+	char ProWin[] = "Process";
+	char CtrWinName[] = "CtrlWin";
+
+	namedWindow(source_window, CV_WINDOW_NORMAL); 	resizeWindow(source_window, 640, 480);
+	namedWindow(ProWin, CV_WINDOW_NORMAL); 	resizeWindow(source_window, 640, 480);
+	namedWindow(CtrWinName, CV_WINDOW_AUTOSIZE);
+
+    /// Create a Trackbar for user to enter threshold
+	createTrackbar("LowTh:", CtrWinName, &lowThreshold, max_lowThreshold);
+	createTrackbar("Ratio:", CtrWinName, &ratio, 3);
+	createTrackbar("BlurKer:", CtrWinName, &iBlurKernelSize, 15);
+
+
+	
+
+	while (1) {
+		src = left_image.clone();
+		// Notes:
+		// Design can be such that 
+		// Step-1: Set the threshold to higher value, ie. Canny (ratio=3, lowThreshold=100), GaussianBlur (Kernel 9x9, 
+		/// Convert image to gray and blur it
+		cvtColor(src, src_gray, CV_BGR2GRAY);
+
+		if (!(iBlurKernelSize & 1))iBlurKernelSize++; // make sure it is kernel size is odd
+
+		// blur the image to reduce noise. Note. Canny also reduces noise by Gausiann.
+		//blur(src_gray, src_blur, Size(iBlurKernelSize, iBlurKernelSize));
+		
+		/// Reduce the noise so we avoid false circle detection
+		// 3rd param: - Ksize The Gaussian kernel size; ksize.width and ksize.height can differ, 
+		//              but they both must be positive and odd. Or, they can be zero’s, 
+		//              then they are computed from sigma
+		// sigmaX     – Gaussian kernel standard deviation in X direction.
+		// sigmaY     – Gaussian kernel standard deviation in Y direction; if sigmaY is zero ... 
+		// borderType – pixel extrapolation method (see borderInterpolate() for details).
+		// 
+		// Note: borderType is set to default.
+		GaussianBlur(src_gray, src_blur, Size(iBlurKernelSize, iBlurKernelSize), 2, 2); // Size(9, 9)
+
+		/// Canny detector
+		Canny(src_blur, src_edge, lowThreshold, lowThreshold * ratio, kernel_size);
+
+		vector<Vec3f> circles;
+
+		/// Apply the Hough Transform to find the circles
+		// 5th Param: Minimum distance between the centers of the detected circles
+		// @param param1 First method - specific parameter.In case of #HOUGH_GRADIENT, it is the higher
+		//	threshold of the two passed to the Canny edge detector(the lower one is twice smaller).
+		//	@param param2 Second method - specific parameter.In case of #HOUGH_GRADIENT, it is the
+		//	accumulator threshold for the circle centers at the detection stage.The smaller it is, the more
+		//	false circles may be detected.Circles, corresponding to the larger accumulator values, will be
+		//	returned first.
+		//	@param minRadius Minimum circle radius.
+		//	@param maxRadius Maximum circle radius.If <= 0, uses the maximum image dimension.If < 0, returns
+		//	centers without finding the radius.
+		//
+		//	@sa fitEllipse, minEnclosingCircle
+		//
+		HoughCircles(src_edge, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows / 10, 200, 100, 30, 0);
+
+		printf("Circles size:%d\n", circles.size());
+		/// Draw the circles detected
+		for (size_t i = 0; i < circles.size(); i++)
+		{
+			Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+			int radius = cvRound(circles[i][2]);
+			printf("Circles[%d] C[%d,%d], R:%d\n", i, cvRound(circles[i][0]), cvRound(circles[i][1]), radius);
+
+			// circle center
+			circle(src, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+
+			// circle outline
+			circle(src, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+		}
+
+
+
+		/// Convert it to color
+		//cvtColor(src_edge, src_gray, CV_BGR2GRAY);
+		//imshow(source_window, src_edge);
+		imshow(source_window, src);
+		imshow(ProWin, src_edge);
+
+		if (waitKey(10) == 27) {
+			break;
+		}
+		//waitKey(0);
+	}
+	//getchar();
+
+	return 0;
+}
